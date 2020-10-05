9a6ebcb627b1f7c129b1c4e9eef5e3b2386dd64c
Michael Read <mrea0002@student.monash.edu>
2020-10-05 12:20:36 +1100

    initial commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+
+  constructor(private router:Router) { }
+
+  routeLogin() {
+    this.router.navigateByUrl('/home');
+  }
+
+}
